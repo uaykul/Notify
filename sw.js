3d78486d154a3d542e2a970dc6b7b09f57c7a72a
uaykul <uaykul@gmail.com>
2023-10-24 16:31:08 +0300

    service updated

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
 self.addEventListener("push", (e) =>{
     const config = {
-        body:"Mesaj icerigi",
+        body: e.data.text(),
         data : {
             dateOfArrival: Date.now(),
             primaryKey: "3"
-        }
+        },
+        icon:"images/logo.png",
+        vibrate: [100, 50, 100]
     }
     e.waitUntil(self.registration.showNotification("Yeni makale", config));
 });
